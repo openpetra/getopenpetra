a906f383e8af984990d18218fbff2e7e99386ad1
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-05-14 06:51:22 +0200

    Debian Buster: we need Mono 6.8 as minimum, due to Newtonsoft Json see https://stackoverflow.com/a/59600806/1632368

--- getopenpetra.sh
+++ getopenpetra.sh
@@ -404,6 +404,14 @@ install_debian()
 		echo 'deb [arch=amd64] https://lbs.solidcharity.com/repos/tpokorra/mono/debian/stretch stretch main' | sudo tee /etc/apt/sources.list.d/mono-tpokorra.list
 		apt-get update
 	fi
+	# For Debian Buster, get Mono packages compiled by SolidCharity.com, because Debian Buster only has Mono 5.18
+	# the packages from Xamarin/Microsoft will be recompiled, that takes too much time during CI
+	if [[ "$VER" == "10" ]]; then
+		apt-get -y install apt-transport-https dirmngr gnupg ca-certificates
+		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0x4796B710919684AC
+		echo 'deb [arch=amd64] https://lbs.solidcharity.com/repos/tpokorra/mono/debian/buster buster main' | sudo tee /etc/apt/sources.list.d/mono-tpokorra.list
+		apt-get update
+	fi
 	if [[ "$install_type" == "devenv" ]]; then
 		apt-get -y install nant mono-devel mono-xsp4 mono-fastcgi-server4 ca-certificates-mono xfonts-75dpi fonts-liberation libgdiplus || exit -1
 	else
