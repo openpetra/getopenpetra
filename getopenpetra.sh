d762237673e927e2c0d25c109346c5f337e28e6a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2026-02-17 20:09:15 +0100

    support CentOS Stream10 and Fedora 43

--- getopenpetra.sh
+++ getopenpetra.sh
@@ -47,7 +47,7 @@
 #
 #	$ curl https://get.openpetra.org | bash -s prod
 #
-# This should work on CentOS Stream 9, Fedora 42
+# This should work on CentOS Stream 10, Fedora 43
 # and Ubuntu 24.04 (Noble Numbat)
 # and Debian 12 (Bookworm).
 # Please open an issue if you notice any bugs.
@@ -678,14 +678,14 @@ install_openpetra()
 		fi
 
 		if [[ "$OS_FAMILY" == "Fedora" ]]; then
-			if [[ "$VER" != "9" && "$VER" != "42" ]]; then
+			if [[ "$VER" != "10" && "$VER" != "43" ]]; then
 				echo "Aborted, Your distro version is not supported: " $OS $VER
 				return 6
 			fi
 		fi
 
 		if [[ "$OS_FAMILY" == "Debian" ]]; then
-			if [[ "$VER" != 12 && "$VER" != "24.04" && "$VER" != "22.04" ]]; then
+			if [[ "$VER" != 12 && "$VER" != "24.04" ]]; then
 				echo "Aborted, Your distro version is not supported: " $OS $VER
 				return 6
 			fi
